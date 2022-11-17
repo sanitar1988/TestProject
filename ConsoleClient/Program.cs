c6f28830c55b9e07bc74be9005606ba0eb0134d3
Андрей Кузнецов <andrey.kuznetzov@yandex.ru>
2022-11-17 14:05:00 +0300

    Change Program class and test runing app

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,30 +6,24 @@ namespace TestProject
     {
         static void Main()
         {
-            //Console.WriteLine("Client start...");
+            Console.WriteLine("Client start...");
 
-            //Console.Write("Enter ipaddress or domain server: ");
-            //string serveradd = Console.ReadLine();
+            Console.Write("Enter ipaddress or domain server: ");
+            string serveradd = Console.ReadLine();
 
-            //Console.Write("Enter port server: ");
-            //int serverport = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter port server: ");
+            int serverport = Convert.ToInt32(Console.ReadLine());
 
-            //Client client = new();
-            //client.Connection(serveradd, serverport);
+            Client client = new();
+            client.Connection(serveradd, serverport);
 
-            //client.ListenServerAsync();
+            client.ListenServerAsync();
 
             for (int i = 0; i < 1000; i++)
             {
                 Console.Write("Enter message: ");
                 string mess = Console.ReadLine();
-                //client.SendMessageAsync(mess);
-
-                byte[] buf = Clear3DES.Encrypt(mess);
-
-                string s = Clear3DES.Decrypt(buf);
-
-                PrintClass.PrintConsole(s);
+                client.SendMessageAsync(mess);
             }
 
             Console.Write("Press enter for exit: ");
