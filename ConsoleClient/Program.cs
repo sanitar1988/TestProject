504bd25773d9de30347c6104fd870006652c710e
Андрей Кузнецов <andrey.kuznetzov@yandex.ru>
2022-11-19 18:51:23 +0300

    Testing serialize in client console

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,15 +1,39 @@
 ﻿using ConsoleClient;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace TestProject
 {
+    [Serializable]
+    public class User
+    {
+        public string _name { get; set; }
+        public int _year { get; set; }
+        public int [] _idFrends { get; set; }
+        public MessageType.EventCommand _type { get; set; }
+
+        public User(string name, int year, int coundfreds)
+        {
+            _name = name;
+            _year = year;
+
+            Random r = new Random();
+            _idFrends = new int[coundfreds]; 
+            for (int i = 0; i < coundfreds; i++)
+            {
+                _idFrends[i] = r.Next(0, 255);  
+            }
+            _type = MessageType.EventCommand.RegInfoUser;
+        }
+    }
+
     class Program
     {
         public static Client client = new();
+        
         static void Main()
         {
-
             AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
 
             //Console.WriteLine("Client start...");
@@ -31,12 +55,28 @@ namespace TestProject
             //    client.SendMessageAsync(mess);
             //}
 
+            BinaryFormatter formatter = new BinaryFormatter();
+            User u = new User("Tom", 29, 100);
+
+            byte[] bmass = new byte[1];
+
+            MemoryStream ms = new MemoryStream();
+            formatter.Serialize(ms, u);
+            bmass = new byte[ms.ToArray().Length];
+            bmass = ms.ToArray();
+
+            ms = new MemoryStream(bmass);
+            User newUser = (User)formatter.Deserialize(ms);
+
+
             Console.Write("Press enter for exit: ");
             Console.ReadLine();
         }
         static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
             string mess = "Byby";
+            Console.WriteLine("Wait 5 sec and close...");
+            Thread.Sleep(5000);
             //byte[] b = new byte[mess.Length + 1];
             //b[0] = (byte)MessageType.EventCommand.UserDisconnected;
 
@@ -44,8 +84,7 @@ namespace TestProject
             //Array.Copy(b, 1, m, 0, m.Length);
 
             //mess = Encoding.UTF8.GetString(b);
-            //client.SendMessageAsync(mess);
-            Thread.Sleep(1000);
+            //client.SendMessageAsync(mess);;
             //client.SendMessageAsync(mess);
         }
 
